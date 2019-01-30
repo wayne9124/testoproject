35d51dc3c28ab75c163297f961805b1f708c6365
wayne9124 <wayne9124@gmail.com>
2019-01-30 17:59:37 +0800

    Update Client.js

--- Client.js
+++ Client.js
@@ -9,29 +9,36 @@ var config = require('./config')
 var client  = mqtt.connect('mqtt://127.0.0.1:1883')
 var dve=({
     "id": null,
-    "name": "one",
-    "password": "123456"
+    "name": "three",
+    "password": "147258"
 })
 
 app.set('Req', config.secret_Req)
 app.set('Reg', config.secret_Reg)
 
 client.on('connect', function () {
+    var string=JSON.stringify(dve);
+    var locatname = JSON.parse(string)
     client.subscribe('Reg-client', function (err) {});///接收token
+    client.subscribe(locatname.name+'ErrorReport', function (err) {})
     var Regpack = jwt.sign(dve, app.get('Reg'), {}) ///打包註冊資料
-    client.publish('Reg-sever', Regpack.toString())///推送註冊資料(加密過的) 2
+    client.publish('Reg-sever', Regpack.toString())///推送註冊資料(加密過的) 
     
 })
 
 client.on('message', function (topic, message){///收到回應
-    tokenClinet=message.toString()
-    console.log(tokenClinet)
-    var Newuser= ({
-        "id": null,
-        "name": "cat",
-        "password": "123456",
-        "token" : tokenClinet
-    })
-    var paloy = jwt.sign(Newuser, app.get('Req'), {}) ///打包註冊資料
-    client.publish('Req-sever', paloy.toString())   
+    if(topic == 'Reg-client'){
+        tokenClinet=message.toString()
+        console.log(tokenClinet)
+        var Newuser= ({
+            "id": null,
+            "name": "bird",
+            "password": "456789",
+            "token" : tokenClinet
+        })
+        var paloy = jwt.sign(Newuser, app.get('Req'), {}) ///打包註冊資料
+        client.publish('Req-sever', paloy.toString()) 
+    } else{
+        console.log(message.toString())
+    }
 })
